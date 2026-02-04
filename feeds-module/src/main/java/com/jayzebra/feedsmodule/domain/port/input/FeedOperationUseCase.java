3b848c397cb7b821b9036dd89b9a199ad35eac27
Jaykumar Gupta <jaykumar.gupta@zebra.com>
2026-02-04 14:48:20 +0530

    Delete feeds-module/src/main/java/com/jayzebra/feedsmodule/domain/port/input directory

--- feeds-module/src/main/java/com/jayzebra/feedsmodule/domain/port/input/FeedOperationUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.jayzebra.feedsmodule.domain.port.input;
-
-import com.jayzebra.feedsmodule.domain.dto.FeedOperationRequestDto;
-
-public interface FeedOperationUseCase {
-    void performFeedOperation(FeedOperationRequestDto requestDto);
-}
